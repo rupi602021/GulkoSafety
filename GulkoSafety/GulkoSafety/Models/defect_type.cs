912fd497bd3aaf23d4d4e8e81eed8b47dffe23c2
rupi602021 <77380749+rupi602021@users.noreply.github.com>
2021-03-12 16:13:58 +0200

    add models
    הוספתי את המחלקות של גיל

--- GulkoSafety/GulkoSafety/Models/defect_type.cs
+++ GulkoSafety/GulkoSafety/Models/defect_type.cs
@@ -7,5 +7,18 @@ namespace GulkoSafety.Models
 {
     public class defect_type
     {
+        int defect_type_num;
+        string type_name;
+
+        public defect_type(int defect_type_num, string type_name)
+        {
+            Defect_type_num = defect_type_num;
+            Type_name = type_name;
+        }
+
+        public int Defect_type_num { get => defect_type_num; set => defect_type_num = value; }
+        public string Type_name { get => type_name; set => type_name = value; }
+
+        public defect_type() { }
     }
 }
--- GulkoSafety/GulkoSafety/Models/project.cs
+++ GulkoSafety/GulkoSafety/Models/project.cs
@@ -7,5 +7,60 @@ namespace GulkoSafety.Models
 {
     public class project
     {
+        int project_num;
+        string name;
+        string company;
+        string address;
+        DateTime start_date;
+        DateTime end_date;
+        int status;
+        string description;
+        float safety_lvl;
+        int project_type_num;
+        string manager_email;
+        string foreman_email;
+
+        public project(int project_num, string name, string company, string address, DateTime start_date, DateTime end_date, int status, string description, float safety_lvl, int project_type_num, string manager_email, string foreman_email)
+        {
+            Project_num = project_num;
+            Name = name;
+            Company = company;
+            Address = address;
+            Start_date = start_date;
+            End_date = end_date;
+            Status = status;
+            Description = description;
+            Safety_lvl = safety_lvl;
+            Project_type_num = project_type_num;
+            Manager_email = manager_email;
+            Foreman_email = foreman_email;
+        }
+
+        public int Project_num { get => project_num; set => project_num = value; }
+        public string Name { get => name; set => name = value; }
+        public string Company { get => company; set => company = value; }
+        public string Address { get => address; set => address = value; }
+        public DateTime Start_date { get => start_date; set => start_date = value; }
+        public DateTime End_date { get => end_date; set => end_date = value; }
+        public int Status { get => status; set => status = value; }
+        public string Description { get => description; set => description = value; }
+        public float Safety_lvl { get => safety_lvl; set => safety_lvl = value; }
+        public int Project_type_num { get => project_type_num; set => project_type_num = value; }
+        public string Manager_email { get => manager_email; set => manager_email = value; }
+        public string Foreman_email { get => foreman_email; set => foreman_email = value; }
+
+        public project() { }
+
+        public List<project> Read()
+        {
+            DBServices dbs = new DBServices();
+            return dbs.ReadProjects();
+        }
+
+        public void Insert()
+        {
+            DBServices dbs = new DBServices();
+            dbs.InsertProject(this);
+        }
     }
 }
